c87146874ff60e584f76d41058162230c82beaf6
Evgenios <eugenioska@gmail.com>
2015-06-06 19:41:16 +0300

    Small chages, pausing and resuming

--- js/countdown.js
+++ js/countdown.js
@@ -1,3 +1,21 @@
-$('#timer').countdown('2020/10/10', function(event) {
-2   $(this).html(event.strftime('%D days %H:%M:%S'));
-3 });
+$(document).ready(function(){
+  var running = true;
+  $('#timer').countdown('25', function(event) {
+    $(this).html(event.strftime('%w weeks %d days %H:%M:%S'));
+  });
+
+  $('#pause').click(function(event){
+    if(running){
+      $('#timer').countdown('pause');
+      $('#pause').html('Resume');
+      running = false;
+    } else {
+      $('#timer').countdown('resume');
+      $('#pause').html('Pause');
+      running = true;
+    }
+    
+
+
+  });
+});
